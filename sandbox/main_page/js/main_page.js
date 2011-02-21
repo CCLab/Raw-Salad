789f896aea9959baef5c6ca41106fb8d09c99724
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2011-02-21 10:25:37 +0100

    General info added

--- sandbox/main_page/js/main_page.js
+++ sandbox/main_page/js/main_page.js
@@ -7,4 +7,13 @@
         $(this).parent().parent().siblings().find('.accordeon').hide();
         $(this).parent().parent().siblings().find('.expert_blog').hide();        
     });
+    $('.dataset-name').hover( 
+        function () {
+            var label = $(this).html();
+            $('#status-bar').html("Kliknij w pasek, by zobaczyć szczegóły kolekcji " + label);
+        },
+        function () {
+            $('#status-bar').html('');
+        }
+    );
 })();
