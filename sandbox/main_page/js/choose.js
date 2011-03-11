25390581a0da6b860a9b4680e3c1bf43d6501d0d
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2011-03-11 16:07:28 +0100

    Still working on the mockups

---
+++ sandbox/main_page/js/choose.js
@@ -0,0 +1,13 @@
+(function () {
+    
+    $('#navigation-img').hover( 
+        function () {
+            $(this).attr('src', './img/choose/navigation_01_over.png');
+        },
+        function () {
+            $(this).attr('src', './img/choose/navigation_01.png');        
+        }
+    );
+
+
+})();
