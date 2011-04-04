e8c9f531270fea06c1f94a4e022d7b073ff25172
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2011-04-04 17:02:18 +0200

    Final design implementation step one a

--- src/rawsalad/site_media/media/js/choose.js
+++ src/rawsalad/site_media/media/js/choose.js
@@ -1,6 +1,21 @@
 (function () {
  
     $('.radio-button').click( function () {
+        var navigation_img = $('#nav-02').attr('src');
+        if( navigation_img === '/site_media/media/img/navigation/01_02.png' )
+        {
+            $('#nav-02').attr('src', '/site_media/media/img/navigation/01_02_active.png' )
+                        .hover( 
+                            function () {
+                                $('#nav-02').attr('src', '/site_media/media/img/navigation/01_02_over.png' ); 
+                                $('#nav-02').css('cursor', 'pointer');
+                            },                     
+                            function () {
+                                $('#nav-02').attr('src', '/site_media/media/img/navigation/01_02_active.png' );                            
+                            });
+            
+        }
+        
         $('.radio-inside')
             .addClass('unselected')
             .removeClass('selected');
@@ -9,13 +24,14 @@
             .addClass('selected')
             .removeClass('unselected');
     });
+    
+    
 
     $('#collection-list').hide();
     
     $('#change-dataset').click( function () {
             $('#collection-list').show();            
     });
-    
     
     $('#collection-list').mouseover( function () {
             $('#collection-list').show();            
