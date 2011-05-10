78cd54dedaf877cedf97cb8797f7e6fd5404de5d
Denis Kolokol <denis_kolokol@ukr.net>
2011-05-10 15:44:51 +0200

    traditional budget update

--- scripts/db/budget/budgtr.py
+++ scripts/db/budget/budgtr.py
@@ -41,81 +41,38 @@ def db_insert(data_bulk, db, collname, clean_first=False):
 
 
 #-----------------------------
-def fill_rbt(budg_data, type_label, db, coll, cleandb):
-    
-    budg_list= []
+def fill_rbt(budg_data, type_label):
+    levels= ['a', 'b', 'c', 'd', 'e', 'f']
+    out= budg_data[:]
 
-    # level 0
-    curr_level= budg_data['level0']
-    for row_doc in curr_level:
-        row_doc['idef']= row_doc['czesc'] # move 'czesc' key to 'idef'
-        if '/' in row_doc['czesc']: # correcting czesc
-            row_doc['idef']= row_doc['idef'].replace('/', '-')
+    for row_doc in out:
+        if row_doc['parent'] == '':
+            row_doc['parent']= None
+        row_doc['level']= levels[row_doc['level']]
+        if '/' in row_doc['czesc']:
             row_doc['czesc']= row_doc['czesc'].partition('/')[0]
-        row_doc['czesc']= int(row_doc['czesc']) #should be int for the possibility to be compared against 'budzet zadaniowy'
-        row_doc['level']= 'a'
-        row_doc['type']= type_label[row_doc['level']] + ' ' + row_doc['idef']
+        row_doc['czesc']= int(row_doc['czesc']) #convert to int for the possibility to be compared against 'budzet zadaniowy'
+        row_doc['type']= type_label[row_doc['level']] + ' ' + str(row_doc['numer'])
         if row_doc['idef'] == '999999':
-            row_doc['type']= 'Ogółem' # specific type
-            del row_doc['level0'] # delete useless key
+            row_doc['type']= 'Total' # specific type
         row_doc['v_nation']= row_doc['dot_sub'] + row_doc['swiad_fiz'] + row_doc['wyd_jednostek'] + row_doc['wyd_majatk'] + row_doc['wyd_dlug'] # recalculate v_nation
         row_doc['v_eu']= row_doc['sw_eu'] + row_doc['wspolfin_eu'] # recalculate v_eu
         row_doc['v_proc_eu']= round(float(row_doc['v_eu']) / float(row_doc['v_total']) * 100, 2) # percentage
         row_doc['v_proc_nation']= round(float(row_doc['v_nation']) / float(row_doc['v_total']) * 100, 2)
-        del row_doc['czesc_orig'] #delete useless keys
-        del row_doc['dzial']
-        del row_doc['rozdzial']
-                
-        budg_list.append(row_doc)
 
-    # level 1
-    curr_level= budg_data['level1']
-    for row_doc in curr_level:
-        row_doc['idef']= row_doc.pop('dzial').replace('.','-') # 'idef' is 'dzial' on the level 1
-        row_doc['parent']= row_doc['czesc']
-        if '/' in row_doc['czesc']:
-            row_doc['parent']= row_doc['czesc'].replace('/', '.')
-            row_doc['czesc']= row_doc['czesc'].partition('/')[0]
-        row_doc['czesc']= int(row_doc['czesc']) #should be int for the possibility to be compared against 'budzet zadaniowy'
-        row_doc['level']= 'b'
-        row_doc['type']= type_label[row_doc['level']] + ' ' + row_doc['idef']
-        row_doc['v_nation']= row_doc['dot_sub'] + row_doc['swiad_fiz'] + row_doc['wyd_jednostek'] + row_doc['wyd_majatk'] + row_doc['wyd_dlug'] # recalculate v_nation
-        row_doc['v_eu']= row_doc['sw_eu'] + row_doc['wspolfin_eu'] # recalculate v_eu
-        row_doc['v_proc_eu']= round(float(row_doc['v_eu']) / float(row_doc['v_total']) * 100, 2) # percentage
-        row_doc['v_proc_nation']= round(float(row_doc['v_nation']) / float(row_doc['v_total']) * 100, 2)
-        del row_doc['czesc_orig'] #delete useless keys
-        del row_doc['rozdzial']
-
-        budg_list.append(row_doc)
-
-    # level 2
-    curr_level= budg_data['level2']
-    for row_doc in curr_level:
-        row_doc['parent']= row_doc['rozdzial'][0:len(row_doc['rozdzial'])-2] #parent is 'dzial', which is the first 2 or 3 digits of 'rozdzial'
-        row_doc['idef']= row_doc.pop('rozdzial').replace('.', '-') # 'idef' is 'rozdzial' on the level 1
-        if '/' in row_doc['czesc']:
-            row_doc['czesc']= row_doc['czesc'].partition('/')[0]
-        row_doc['czesc']= int(row_doc['czesc']) #should be int for the possibility to be compared against 'budzet zadaniowy'
-        row_doc['level']= 'c'
-        row_doc['leaf']= True
-        row_doc['type']= type_label[row_doc['level']] + ' ' + row_doc['idef']
-        row_doc['v_nation']= row_doc['dot_sub'] + row_doc['swiad_fiz'] + row_doc['wyd_jednostek'] + row_doc['wyd_majatk'] + row_doc['wyd_dlug'] # recalculate v_nation
-        row_doc['v_eu']= row_doc['sw_eu'] + row_doc['wspolfin_eu'] # recalculate v_eu
-        row_doc['v_proc_eu']= round(float(row_doc['v_eu']) / float(row_doc['v_total']) * 100, 2) # percentage
-        row_doc['v_proc_nation']= round(float(row_doc['v_nation']) / float(row_doc['v_total']) * 100, 2)
-        del row_doc['czesc_orig'] #delete useless keys
-        del row_doc['dzial']
-
-        budg_list.append(row_doc)
-
-    print '-- ', db_insert(budg_list, db, coll, cleandb), 'records inserted'
+    # filling out 'leaf'
+    budg_list= out[:]
+    for bd in out:
+        for bl in budg_list:
+            if bl['parent'] == bd['idef']:
+                bd['leaf']= False
+                break
+    return out
 
 
 #-----------------------------
 def csv_parse(csv_read, schema):
-    level0= []
-    level1= []
-    level2= []
+    out= []
 
     dbkey_alias= schema['alias'] # dict of aliases -> document keys in db
     dbval_types= schema['type'] # dict of types -> values types in db
@@ -156,45 +113,36 @@ def csv_parse(csv_read, schema):
                     except:
                         dict_row[new_key] = field # no, it is a string
                 #additional fields
-                dict_row['parent']= None
-                dict_row['idef']= None
                 dict_row['type']= None
-                dict_row['leaf']= False
+                dict_row['leaf']= True
                 dict_row['v_nation']= 0
                 dict_row['v_eu']= 0
                 dict_row['v_proc_eu']= 0
                 dict_row['v_proc_nation']= 0
-                
 
                 i += 1
 
-            if dict_row['level'] == 0:
-                level0.append(dict_row)
+            if dict_row['level'] == 0:                
                 for k in dbkeys_summ: #totalling here all the summarizable values
                     total_dict[k]= total_dict[k] + dict_row[k]
-            elif dict_row['level'] == 1:
-                level1.append(dict_row)
-            else:
-                level2.append(dict_row)
+            out.append(dict_row)
 
     #complete 'total' dict (some of the keys will anyway be deleted on the 2nd pass)
-    total_dict['czesc']= '999999'
+    total_dict['idef']= '999999'
+    total_dict['czesc']= '0'
     total_dict['pozycja']= 0
-    total_dict['name']= ''
+    total_dict['name']= 'Ogółem'
+    total_dict['numer']= ''
     total_dict['parent']= None
-    total_dict['level0']= 0
     total_dict['leaf']= True
-    total_dict['czesc_orig']= None
-    total_dict['dzial']= 0
-    total_dict['rozdzial']= 0
+    total_dict['level']= 0
     total_dict['v_nation']= 0
     total_dict['v_eu']= 0
     total_dict['v_proc_eu']= 0
     total_dict['v_proc_nation']= 0
 
-    level0.append(total_dict)
+    out.append(total_dict)
 
-    out= dict({'level0': level0, 'level1': level1, 'level2': level2})
     return out
 
 
@@ -281,7 +229,8 @@ if __name__ == "__main__":
 
         print '-- inserting data into '+ dbname +'.'+ dbname
         mongo_connect.start_request()
-        obj_rep= fill_rbt(obj_parsed, meta_level_label, work_db, collectname, clean_db) # processing and inserting the data
+        obj_rep= fill_rbt(obj_parsed, meta_level_label) # processing and inserting the data
+        print "-- %d records inserted" % db_insert(obj_rep, work_db, collectname, clean_db)
 
         #meta info
         meta_name= meta_info['name']
