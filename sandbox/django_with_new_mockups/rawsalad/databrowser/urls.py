05b321d79dd551073b9d53790c549338da7cd464
Denis Kolokol <denis_kolokol@ukr.net>
2011-05-17 01:58:13 +0200

    deleted old src

--- sandbox/django_with_new_mockups/rawsalad/databrowser/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns( 'databrowser.views',
-    (r'^$', 'redirect' ),
-    (r'^app/$', 'app_page' ),
-)
--- sandbox/django_with_new_mockups/rawsalad/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from django.conf import settings
-
-## ---> Static linkage to libs to be done
-urlpatterns = patterns('',
-    (r'^site_media/media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    (r'^', include('rawsalad.databrowser.urls')),
-)
