e38f746d229bcda7990e13aeeea2282435e00820
Denis Kolokol <denis_kolokol@ukr.net>
2011-05-20 18:51:57 +0200

    working db API v0

--- src/rawsalad/databrowser/rawsdbapi.py
+++ src/rawsalad/databrowser/rawsdbapi.py
@@ -3,9 +3,9 @@
 import pymongo
 from ConfigParser import ConfigParser
 
-conn_schema= 'md_budg_scheme'
-conf_filename= "/Users/deniskolokol/dev/rawsalad/repo/rawsdata.conf"
-#global conf_filename= "/home/cecyf/www/projects/rawsalad/rawsdata.conf" 
+conn_schema= "md_budg_scheme"
+nav_schema= "ms_nav"
+conf_filename= "/home/cecyf/www/projects/rawsalad/src/rawsalad/site_media/media/rawsdata.conf"
 
 
 #-----------------------------
@@ -15,15 +15,29 @@ def get_db_connect(dbtype):
         'basedir': conf_filename
     }
     cfg= ConfigParser(defaults)
+
     cfg.read(conf_filename)
-    connect_dict['host']= cfg.get(dbtype,'host')
-    connect_dict['port']= cfg.getint(dbtype,'port')
-    connect_dict['database']= cfg.get(dbtype,'database')
-    connect_dict['username']= cfg.get(dbtype,'username')
-    try:
-        connect_dict['password']= cfg.get(dbtype,'password')
+    
+    success= True
+    try: # check if we can read conf file
+        connect_dict['host']= cfg.get(dbtype,'host')
     except:
-        connect_dict['password']= '' # password must be an instance of basestring
+        success= False
+
+    if success: # all OK, fill the rest
+        connect_dict['port']= cfg.getint(dbtype,'port')
+        connect_dict['database']= cfg.get(dbtype,'database')
+        connect_dict['username']= cfg.get(dbtype,'username')
+        try:
+            connect_dict['password']= cfg.get(dbtype,'password')
+        except:
+            connect_dict['password']= '' # password must be instance of basestring
+    else: # can't read conf file - filling defaults
+        connect_dict['host']= 'localhost'
+        connect_dict['port']= 27017
+        connect_dict['database']= 'rawsdoc00'
+        connect_dict['username']= 'readonly'
+        connect_dict['password']= ''        
 
     return connect_dict
 
@@ -31,7 +45,9 @@ def get_db_connect(dbtype):
 
 #-----------------------------
 def get_metadata_full(ds_id, ps_id, dbase):
-    # returns complete set of metadata (with "sort", "aux" and "query")
+    """
+    returns complete set of metadata (with "sort", "aux" and "query")
+    """
     metadata_full= dbase[conn_schema].find_one({ 'dataset': ds_id, 'idef' : ps_id }, { '_id' : 0 })
     return metadata_full
 
@@ -39,34 +55,37 @@ def get_metadata_full(ds_id, ps_id, dbase):
 
 #-----------------------------
 def get_metadata(dataset_id, perspective_id):
-    # returns metadata used for representation (without "sort", "aux" and "query")
-    #
-    # WARNING! there should be optional parameter - connection
-    # calling this func from views.py without connection means that we have to do get_db_connect first
-    # but if connection is given, just use it for extraction
+    """
+    returns metadata used for representation (without "sort", "aux" and "query")
+    """
 
-    ds= int(dataset_id)
-    ps= int(perspective_id)
+    md_ds= int(dataset_id)
+    md_ps= int(perspective_id)
 
     # connection details
     dsn= get_db_connect('mongodb')
     connect= pymongo.Connection(dsn['host'], dsn['port'])
-    db= connect[dsn['database']]
-    db.authenticate(dsn['username'], dsn['password'])
+    md_db= connect[dsn['database']]
+    md_db.authenticate(dsn['username'], dsn['password'])
 
     # EXTRACT metadata
-    md= get_metadata_full(ds, ps, db)
+    mdata= get_metadata_full(md_ds, md_ps, md_db)
     keys_to_delete= ['sort', 'query', 'aux', 'batchsize']
     for k in keys_to_delete:
-        if k in md:
-            del md[k]
+        if k in mdata:
+            del mdata[k]
 
-    return md
+    return mdata
+
 
 
 #-----------------------------
 def extract_docs(dataset_id, perspective_id, query_aux):
-    # extracts initial data - level 'a' of a given collection
+    """
+    extracts data for given dataset and perspective
+    query_aux represents special cases
+    (for example, extract only highest level or children of a parent)
+    """
 
     ds= int(dataset_id)
     ps= int(perspective_id)
@@ -121,6 +140,35 @@ def extract_docs(dataset_id, perspective_id, query_aux):
 
 
 
-# aa= {'parent':'15'}
-# for ii in extract_docs(1, 1, aa):
-#     print ii
+#-----------------------------
+def extract_nav(keys_aux=None, query_aux=None):
+    """
+    extracts navigation data
+    keys_aux represents special cases like extract only dataset without perspectives
+    query_aux represents special cases like extract specified dataset with perspectives
+    """
+
+    # connection details
+    dsn= get_db_connect('mongodb')
+    connect= pymongo.Connection(dsn['host'], dsn['port'])
+    db= connect[dsn['database']]
+    db.authenticate(dsn['username'], dsn['password'])
+
+    if keys_aux is None:
+        keys_aux= {}
+    if query_aux is None:
+        query_aux= {}
+
+    nav_select_columns= {'_id':0} # _id is never returned
+    nav_select_columns.update(keys_aux)
+
+    cond_query= {} # query conditions
+    cond_query.update(query_aux) # additional query, depends on the call
+
+    cursor_data= db[nav_schema].find(cond_query, nav_select_columns)
+
+    out= []
+    for row in cursor_data:
+        out.append(row)
+
+    return out
