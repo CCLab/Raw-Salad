8ccf46bdf4ae421989139c4d0e5e6db407670784
Denis Kolokol <denis_kolokol@ukr.net>
2011-05-20 18:52:30 +0200

    extract data

--- src/rawsalad/databrowser/views.py
+++ src/rawsalad/databrowser/views.py
@@ -4,11 +4,8 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.template import Context, loader
 from django.utils import simplejson as json
-
-# should be removed after data is imported from db
-from data import new_rows
-from data import perspective
-#import rawsdbapi
+# DK
+import rawsdbapi
 
 #import pymongo
 
@@ -72,13 +69,12 @@ def get_node( data ):
     per_nr = data["per_nr"]
     par_id = data["par_id"]
     add_columns = data["add_columns"]
+
+    # DK
     # getting data from db
     return_data = []
-    for row in new_rows:
-        if row["parent"] == par_id:
-            return_data.append(row)
-#    parent_data_query= { 'parent': par_id }
-#    return_data= rawsdbapi.extract_docs(col_nr, per_nr, parent_data_query)
+    parent_data_query= { 'parent': par_id }
+    return_data= rawsdbapi.extract_docs(col_nr, per_nr, parent_data_query)
 
     json_data = json.dumps( return_data )
 
@@ -88,16 +84,11 @@ def get_init_data( data ):
     col_nr = data["col_nr"]
     per_nr = data["per_nr"]
     
-    return_data = {}
-    return_data["perspective"] = perspective
-    return_data["rows"] = []
-    for row in new_rows:
-        if row["level"] == "a":
-            return_data["rows"].append(row)
-            
-#    return_data["perspective"]= rawsdbapi.get_metadata(col_nr, per_nr)
-#    initial_data_query= { 'level': 'a' }
-#    return_data["rows"]= rawsdbapi.extract_docs(col_nr, per_nr, initial_data_query)
+    # DK
+    return_data = {}            
+    return_data['perspective']= rawsdbapi.get_metadata(col_nr, per_nr)
+    initial_data_query= { 'level': 'a' }
+    return_data["rows"]= rawsdbapi.extract_docs(col_nr, per_nr, initial_data_query)
 
     json_data = json.dumps( return_data )
     
@@ -131,136 +122,26 @@ def redirect( request ):
     
     
 def get_meta_data():
-    return { 
-              'collections': [
-                    {
-                        'name': 'Budżet centralny',
-                        'description': 'Ustawa budżetowa w kilku odsłonach'
-                    },
-                    {
-                        'name': 'Budżet środków europejskich',
-                        'description': 'Udział środków europejskich w budżecie centralnym Polski'
-                    },
-                    {
-                        'name': 'Fundusze celowe i agencje narodowe',
-                        'description': 'Części kosztowe budżetów wszystkich funduszy celowych i agencji narodowych'
-                    },
-                    {
-                        'name': 'Narodowy Fundusz Zdrowia',
-                        'description': 'Budżet Narodowego Funduszu Zdrowia z rozbiciem na ośrodki wojewódzkie'
-                    },
-                    {
-                        'name': 'Krajowy Fundusz Drogowy',
-                        'description': 'Dane finansowe Krajowego Funduszu Drogowego'
-                    }                                        
-                ]
-            }
+    # DK
+    datasets= {'collections':None}
+    # return only the highest level
+    datasets['collections']= rawsdbapi.extract_nav(keys_aux={'perspectives':0})
+
+    return datasets
             
             
 def get_perspectives( col_nr ):
+    # DK
+    dataset_coll= {}
+    # dataset
+    keys_dict={ 'name':1, 'idef':1 }
+    query_dict={ 'idef':int(col_nr) }
+    dataset_coll['collection']= rawsdbapi.extract_nav(keys_dict, query_dict)[0]
+    dataset_coll['collection']['number']= dataset_coll['collection'].pop('idef')
+    # its perspectives
+    keys_dict={ 'perspectives':1 }
+    query_dict={ 'idef':int(col_nr) }
+    persp_dict= rawsdbapi.extract_nav(keys_dict, query_dict)[0]
+    dataset_coll.update(persp_dict)
 
-    if col_nr == '0':
-        return {
-            'collection': {
-                'name': 'Budżet centralny',
-                'number': 1
-                },
-            'perspectives': [
-                {
-                    'name': 'Budżet tradycyjny',
-                    'description': 'Ustawa budżetowa as such',
-                    'issue': [ 2011 ]
-                    },
-                {
-                    'name': 'Budżet zadaniowy',
-                    'description': 'Dwadzieścia dwie funkcje państwa',
-                    'issue': [ 2011, 2012 ]
-                    },
-                {
-                    'name': 'Budżet instytucjonalny',
-                    'description': 'Wszyscy dysponenci ustawy budżetowej',
-                    'issue': [ 2011, 2012 ]
-                    }
-                ]
-            }
-    elif col_nr == '1':
-        return {
-            'collection': {
-                'name': 'Środki europejskie',
-                'number': 2
-                },
-            'perspectives': [
-                {
-                    'name': 'Budżet środków europejskich',
-                    'description': 'Dane dotyczące środków europejskich na szczeblu centralnym',
-                    'issue': [ 2011 ]
-                    },
-                {
-                    'name': 'Środki europejskie a ustawa budżetowa ',
-                    'description': 'Przegląd relacji między środkami europejskimi a ustawą budżetową',
-                    'issue': [ 2011 ]
-                    }
-                ]
-            }    
-    elif col_nr == '2':
-        return {
-            'collection': {
-                'name': 'Fundusze celowe i agencje',
-                'number': 3
-                },
-            'perspectives': [
-                {
-                    'name': 'Dane budżetowe w układzie zadaniowym',
-                    'description': 'Z podziałem na funkcje, zadania i podzadania',
-                    'issue': [ 2011 ]
-                    },
-                {
-                    'name': 'Dane budżetowe w układzie tradycyjnym',
-                    'description': 'Odpowiadające klasycznemu układowi budżetu państwa',
-                    'issue': [ 2011 ]
-                    }
-                ]
-            }
-    elif col_nr == '3':
-        return {
-            'collection': {
-                'name': 'Narodowy Fundusz Zdrowia',
-                'number': 4
-                },
-            'perspectives': [
-                {
-                    'name': 'Dane zagregowane',
-                    'description': 'Dane szczebla centralnego',
-                    'issue': [ 2011 ]
-                    },
-                {
-                    'name': 'Dane ośrodków regionalnych',
-                    'description': 'Dane budżetowe ośrodków regionalnych NFZ',
-                    'issue': [ 2011 ]
-                    }
-                ]
-            }
-    else:
-        return {
-            'collection': {
-                'name': 'Krajowy Fundusz Drogowy',
-                'number': 5
-                },
-            'perspectives': [
-                {
-                    'name': 'Dane zagregowane',
-                    'description': 'Ogólne dane o wydatkach KFD',
-                    'issue': [ 2011 ]
-                    }
-                ]
-            }
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    return dataset_coll
