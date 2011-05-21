89deae8f003fade67fd5b854664fc16c1d21e38c
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2011-05-21 15:47:39 +0200

    Urls bug fixed

--- src/rawsalad/papi/urls.py
+++ src/rawsalad/papi/urls.py
@@ -2,21 +2,21 @@ from django.conf.urls.defaults import *
 import papi
 
 urlpatterns = patterns( '',
-    (r'^api/$', 'get_datasets' ),
-    (r'^api/meta/$', 'get_datasets_meta' ),
-    (r'^api/dataset/$', 'get_datasets' ),
-    (r'^api/dataset/meta/$', 'get_datasets_meta' ),
+    (r'^$', 'get_datasets' ),
+    (r'^meta/$', 'get_datasets_meta' ),
+    (r'^dataset/$', 'get_datasets' ),
+    (r'^dataset/meta/$', 'get_datasets_meta' ),
     
-    (r'api/dataset/(?P<dataset>\d+)/$', 'get_views' ),
-    (r'api/dataset/(?P<dataset>\d+)/meta/$', 'get_views_meta' ),
-    (r'api/dataset/(?P<dataset>\d+)/view/$', 'get_views' ),
-    (r'api/dataset/(?P<dataset>\d+)/view/meta/$', 'get_views_meta' ),
+    (r'dataset/(?P<dataset>\d+)/$', 'get_views' ),
+    (r'dataset/(?P<dataset>\d+)/meta/$', 'get_views_meta' ),
+    (r'dataset/(?P<dataset>\d+)/view/$', 'get_views' ),
+    (r'dataset/(?P<dataset>\d+)/view/meta/$', 'get_views_meta' ),
     
-    (r'api/dataset/(?P<dataset>\d+)/view/(?P<view>\d+)/$', 'get_issues' ),
-    (r'api/dataset/(?P<dataset>\d+)/view/(?P<view>\d+)/meta/$', 'get_issues_meta' ),
-    (r'api/dataset/(?P<dataset>\d+)/view/(?P<view>\d+)/issue/$', 'get_issues' ),
-    (r'api/dataset/(?P<dataset>\d+)/view/(?P<view>\d+)/issue/meta/$', 'get_issues_meta' ),
+    (r'dataset/(?P<dataset>\d+)/view/(?P<view>\d+)/$', 'get_issues' ),
+    (r'dataset/(?P<dataset>\d+)/view/(?P<view>\d+)/meta/$', 'get_issues_meta' ),
+    (r'dataset/(?P<dataset>\d+)/view/(?P<view>\d+)/issue/$', 'get_issues' ),
+    (r'dataset/(?P<dataset>\d+)/view/(?P<view>\d+)/issue/meta/$', 'get_issues_meta' ),
     
-    (r'api/dataset/(?P<dataset>\d+)/view/(?P<view>\d+)/issue/(?P<issue>\d+)/(?P<path>\.*)/meta/$', 'get_data_meta' ),
-    (r'api/dataset/(?P<dataset>\d+)/view/(?P<view>\d+)/issue/(?P<issue>\d+)/(?P<path>\.*)/$', 'get_data' ),
+    (r'dataset/(?P<dataset>\d+)/view/(?P<view>\d+)/issue/(?P<issue>\d+)/(?P<path>\.*)/meta/$', 'get_data_meta' ),
+    (r'dataset/(?P<dataset>\d+)/view/(?P<view>\d+)/issue/(?P<issue>\d+)/(?P<path>\.*)/$', 'get_data' ),
 )
