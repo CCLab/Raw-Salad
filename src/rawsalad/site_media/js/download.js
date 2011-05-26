86fb6f5303b58729d4177196a2a108e7e3241ab1
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2011-05-26 15:56:31 +0200

    Download module v.0.1

--- src/rawsalad/site_media/js/download.js
+++ src/rawsalad/site_media/js/download.js
@@ -3,10 +3,17 @@ var _download = (function () {
     var that = {};
     
     that.current_sheet = function () {
+        var i;
         var data;
         var sheet = _store.active_sheet();
         var html = [];
         var form;
+        var columns = _utils.filter( function ( e ) {
+                return e['basic'] === true;
+           }, _store.active_columns() );
+           
+        
+        
         
         html.push( '<form action="/download/" method="post" name="input">' );
         html.push( '<input type="text" name="sheet" />' );
