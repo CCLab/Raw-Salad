81c37b6ad8863416c5ea500bcf60ce8d9b812dd6
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2011-05-27 13:14:31 +0200

    Download gives hierarchical csv files

--- src/rawsalad/site_media/js/download.js
+++ src/rawsalad/site_media/js/download.js
@@ -7,32 +7,56 @@ var _download = (function () {
         var data = '';
         var html = [];
         var form;
-        var columns = _utils.filter( function ( e ) {
+        var columns, rows;
+        
+        var add_children = function ( parent ) {
+            var i;
+            var children = _utils.filter( function ( e ) {
+                return e['parent'] === parent;
+            }, _store.active_rows() );
+            
+            if( children.length === 0 ) {
+                return;
+            }
+            
+            for( i = 0; i < children.length; i += 1 ) {
+                data += children[i]['idef'] + ';';
+                if( !!children[i]['parent'] === false ) {
+                    data += ';'
+                }
+                else {
+                    data += children[i]['parent'] + ';';
+                }
+                data += children[i]['level'] + ';';
+
+                for( j = 0; j < columns.length; j += 1 ) {
+                    data += children[i][ columns[j]['key'] ];
+                    data += ';';
+                }
+                data += '|';
+                
+                add_children( children[i]['idef'] );
+            }
+        };
+
+
+        columns = _utils.filter( function ( e ) {
                 return e['basic'] === true;
            }, _store.active_columns() );
         
         _assert.not_equal( columns.length, 0,
                            ">> DOWNLOAD <br/>Columns length === 0" );
-     
-        var rows = _store.active_rows();
-        
-        _assert.not_equal( rows.length, 0,
-                           ">> DOWNLOAD <br/>Rows length === 0" );
 
+        data += 'ID;Rodzic;Poziom;';
+             
         for( i = 0; i < columns.length; i += 1 ) {
             data += columns[i]['label'];
             data += ';';
         }
         data += '|';
      
-        for( i = 0; i < rows.length; i += 1 ) {
-            for( j = 0; j < columns.length; j += 1 ) {
-                data += rows[i][ columns[j]['key'] ];
-                data += ';';
-            }
-            data += '|';
-        }
-     
+        add_children( null );
+        
         _assert.not_equal( data, '', 
                            ">> DOWNLOAD <br/>Data string empty" );
         
