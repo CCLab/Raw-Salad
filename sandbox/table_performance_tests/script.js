2582079dc3f2b8eef1771b2fae22836b2a2f82f9
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2011-06-16 21:27:46 +0200

    New table working fast and smoothly

--- sandbox/table_performance_tests/script.js
+++ sandbox/table_performance_tests/script.js
@@ -1,25 +1,53 @@
 (function () {
-    var i, f;
-    var html = [];
+    // good old store...
+    var store = [];
 
-    make_table();
+    // execution ground
+    init_table( full_data.filter( function ( e ) {
+        return e['level'] === 'a';
+    }));
+
     make_zebra();
 
-    function kill_children( id ) {
-        $('.'+id).each( function () {
-            kill_children( $(this).attr('id') );
-            $(this).remove();
+    $('#clone').click( function ( event ) {
+        var start = new Date().getTime();
+        var store_copy = store.slice();
+        store = [];
+
+        $('tbody').html('');
+
+        make_table( store_copy );
+        make_zebra();
+        console.log( new Date().getTime() - start );
+    });
+
+
+    // functions definitions
+    function kill_children( id, what ) {
+        $('tr.'+id).each( function () {
+            kill_children( $(this).attr('id'), what );
+
+            if( what === 'hide' ) {
+                $(this).hide();
+            }
+            else {
+                $(this).show();
+            }
         });
     }
 
-    function make_table( level ) {
-        var level = level || 'a';
-        var next_letter = function( letter ) {
-            var number = letter.charCodeAt( 0 );
-            return String.fromCharCode( number + 1 );
-        };
 
-        var nodes = full_data.filter( function ( e ) {
+    function next_letter( letter ) {
+        var number = letter.charCodeAt( 0 );
+
+        return String.fromCharCode( number + 1 );
+    }
+
+
+    function make_table( list, level ) {
+        var level = level || 'a';
+
+        var nodes = list.filter( function ( e ) {
             return e['level'] === level;
         });
 
@@ -34,7 +62,7 @@
             add_rows( nodes );
         }
 
-        make_table( next_letter( level ));
+        make_table( list, next_letter( level ));
     }
 
 
@@ -53,63 +81,70 @@
 
 
     function init_table( list ) {
-        var i;
-        var html = [];
-        var node;
-        var row;
+        var i = 0;
 
-        for( i = 0; i < list.length; ++i ) {
-            node = list[i];
-            html =[];
-
-            html.push( '<tr id="', node['idef'], '" class="a">' );
-            html.push( '<td class="type click">', node['type'], '</td>' );
-            html.push( '<td class="name">', node['name'], '</td>' );
-            html.push( '<td class="value">', Tools.money(node['v_eu']), '</td>' );
-            html.push( '<td class="value">', Tools.money(node['v_nation']), '</td>' );
-            html.push( '<td class="value">', Tools.money(node['v_total']), '</td>' );
-            html.push( '</tr>' );
-
-            (function ( id ) {
-                row = $(html.join('')).click( function ( event ) {
-                    kill_children( id );
-                    make_zebra();
-                });
-            })( node['idef'] );
-
-            $('tbody').append( row );
+        for( ; i < list.length; ++i ) {
+            $('tbody').append( generate_row( list[i] ));
         }
+        make_zebra();
     }
 
 
     function add_rows( list ) {
-        var i;
-        var html;
-        var node;
-        var row;
+        var i = list.length - 1;
 
-        for( i = list.length-1; i >= 0; i -= 1 ) {
-            node = list[i];
-            html = [];
-
-            html.push( '<tr id="', node['idef'], '" ' );
-            html.push( 'class="', node['level'], ' ', node['parent'], '">' );
-            html.push( '<td class="type', node['leaf'] ? '">' : ' click">' );
-            html.push( node['type'], '</td>' );
-            html.push( '<td class="name">', node['name'], '</td>' );
-            html.push( '<td class="value">', Tools.money(node['v_eu']), '</td>' );
-            html.push( '<td class="value">', Tools.money(node['v_nation']), '</td>' );
-            html.push( '<td class="value">', Tools.money(node['v_total']), '</td>' );
-            html.push( '</tr>' );
-
-            (function ( id ) {
-                row = $(html.join('')).click( function ( event ) {
-                    kill_children( id );
-                    make_zebra();
-                });
-            })( node['idef'] );
-
-            $('#'+node['parent']).after( row );
+        for( ; i >= 0; i -= 1 ) {
+            $('#'+list[i]['parent']).after( generate_row( list[i] ));
         }
     }
+
+    function generate_row( node ) {
+        var html = [];
+        var row;
+
+        // append this node to the current store
+        store.push( node );
+
+        // row definition
+        html.push( '<tr id="', node['idef'], '" ' );
+        html.push( 'data-open="false" ' );
+        html.push( 'class="', node['level'], ' ', node['parent'],'">' );
+
+        // cells definition
+        html.push( '<td class="type', node['leaf'] ? '">' : ' click">' );
+        html.push( node['type'], '</td>' );
+        html.push( '<td class="name">', node['name'], '</td>' );
+        html.push( '<td class="value">', Tools.money(node['v_eu']), '</td>' );
+        html.push( '<td class="value">', Tools.money(node['v_nation']), '</td>' );
+        html.push( '<td class="value">', Tools.money(node['v_total']), '</td>' );
+
+        html.push( '</tr>' );
+
+        // create & arm row
+        row = $( html.join('') );
+        row.click( function ( event ) {
+            if( $(this).attr( 'data-open' ) === 'false' ) {
+
+                if( $('.'+node['idef']).length !== 0 ) {
+                    kill_children( node['idef'], 'show' );
+                }
+                else {
+                    var children = full_data.filter( function ( e ) {
+                        return e['parent'] === node['idef'];
+                    });
+
+                    add_rows( children );
+                }
+                $(this).attr( 'data-open', 'true' );
+            }
+            else {
+                kill_children( node['idef'], 'hide' );
+                $(this).attr( 'data-open', 'false' );
+            }
+            make_zebra();
+        });
+
+        return row;
+    }
+
 })();
