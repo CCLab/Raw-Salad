e945b27e59671b4e8b6d64ee4913a44ebecdaaad
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2011-07-11 13:02:47 +0200

    urls bug fixed

--- src/rawsalad/databrowser/urls.py
+++ src/rawsalad/databrowser/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns( 'databrowser.views',
     (r'^prepare_data/$', 'prepare_data' ),
     (r'^download/$', 'download_data' ),
-    (r'^search/$', 'serach_data' ),
     (r'^$', 'redirect' ),
+    (r'^app/search/$', 'serach_data' ),
     (r'^app/$', 'app_page' ),
 )
