222d5adadb4829abcf2660d80e7f8ac5b97d8bbf
Bartosz Stalewski <bartekstalewski@gmail.com>
2011-07-12 11:00:45 +0200

    Django function for getting searched data added

--- src/rawsalad/databrowser/urls.py
+++ src/rawsalad/databrowser/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns( 'databrowser.views',
     (r'^download/$', 'download_data' ),
     (r'^$', 'redirect' ),
     (r'^app/search/$', 'search_data' ),
+    (r'^app/get_searched/$', 'get_searched_data' ),
     (r'^app/$', 'app_page' ),
 )
