4ba4a6e0068505bb3222e14474f0e4541bb2f3f2
Jerzy de Mezer <jerzydem@gmail.com>
2011-07-20 12:56:22 +0200

    add _store.next_sheet_name() 2

--- src/rawsalad/site_media/js/store.js
+++ src/rawsalad/site_media/js/store.js
@@ -326,7 +326,7 @@ var _store = (function () {
                     next_num = sheet_name[1] + 1;
                    }; 
             }
-        }
+        });
          return 'Arkusz ' + next_num; 
         
     };
