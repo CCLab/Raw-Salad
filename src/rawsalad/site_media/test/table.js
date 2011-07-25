086322298c6e0c7216ea8ca1938da24be9774d3f
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2011-07-25 15:37:08 +0200

    Bug fixed

--- src/rawsalad/site_media/test/table.js
+++ src/rawsalad/site_media/test/table.js
@@ -197,7 +197,7 @@ var _table = (function () {
             html.push( column['type'] );
             html.push( !data['leaf'] && column['key'] === 'type' ? ' click">' : '">' );
             html.push( data[column['key']] );
-            if( data['info'] !== null ) {
+            if( !!data['info'] ) {
                 html.push( '<img src="/site_media/img/info_small.png" border="0"/>' );
             }
             html.push( '</td>' );
