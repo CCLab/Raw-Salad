95a51ff35827393f4678fc23988bf095b0efefe7
Jerzy de Mezer <jerzydem@gmail.com>
2011-07-26 14:43:12 +0200

    add rename sheet 4

--- src/rawsalad/site_media/test/tools.js
+++ src/rawsalad/site_media/test/tools.js
@@ -584,6 +584,17 @@ var _tools = (function () {
     }
 
     function prepare_rename_sheet_interface() {
+        $('#app-tb-tl-rename-form')
+            .submit( function () {
+                var old_sheet_name = _store.active_sheet_name();
+                var new_sheet_name = $('#app-tb-tl-rename-input').val();
+                if ( old_sheet_name !== new_sheet_name ){
+                    alert("Set new sheet name");        
+                }
+                $('#app-tb-tl-rename-form').hide();
+                $('#app-tb-tl-title').show();         
+            });
+
         $('#app-tb-tl-rename-button')
             .click( function () {
                 var active_sheet_name = _store.active_sheet_name();
@@ -592,17 +603,7 @@ var _tools = (function () {
                 $('#app-tb-tl-rename-form').show();
                 $('#app-tb-tl-rename-button').click( function () {
                     $('#app-tb-tl-rename-form').submit();
-                    } );
-                });
-        $('#app-tb-tl-rename-form')
-            .submit( function () {
-                var old_sheet_name = _store.active_sheet_name();
-                var new_sheet_name = $('#app-tb-tl-rename-input').val();
-                if ( old_sheet_name === new_sheet_name ){
-                    alert("Set new sheet name");        
-                }
-                $('#app-tb-tl-rename-form').hide();
-                $('#app-tb-tl-title').show();         
+                } );
             });
     }
 
