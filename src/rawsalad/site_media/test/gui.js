0b64440098b725152d5135bce31ba37eb7575d19
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2011-08-01 07:53:23 +0200

    Preventing data lost on back button

--- src/rawsalad/site_media/test/gui.js
+++ src/rawsalad/site_media/test/gui.js
@@ -31,6 +31,18 @@ var _gui = (function () {
 
     that.init_gui = function ( hide_panel ) {
 
+        window.onbeforeunload = function ( e ) {
+            e = e || window.event;
+
+            // For IE and Firefox prior to version 4
+            if (e) {
+                e.returnValue = 'Any string';
+            }
+
+            // For Safari
+            return 'Any string';
+        };
+
         // arm top menu
         $('#top')
             .find('li')
