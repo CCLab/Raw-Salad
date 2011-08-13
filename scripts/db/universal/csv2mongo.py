45a7614538f3ed308f5e01636e32f8a54ca3d324
denis kolokol <deniskolokol@denis-kolokols-MacBook-Pro-15.local>
2011-08-13 13:27:55 +0200

    example of csv import to mongodb

--- scripts/db/universal/csv2mongo.py
+++ scripts/db/universal/csv2mongo.py
@@ -40,7 +40,6 @@ def get_db_connect(fullpath, dbtype):
 
     return connect_dict
 
-
 #-----------------------------
 def db_insert(data_bulk, db, collname, clean_first=False):
     collect= db[collname]
@@ -55,9 +54,69 @@ def db_insert(data_bulk, db, collname, clean_first=False):
         collect.insert(data_bulk)
     except Exception as e:
         print 'Unable to remove data from the collection:\n %s\n' % e
-        
+
     return collect.find().count()
 
+#-----------------------------
+def sort_format(src):
+    """
+    format 1-2-3... to 00001-00002-00003...
+    src should be convertable to int
+    """
+    src_list= src.split('-')
+    res_list= []
+    for elm in src_list:
+        try:
+            res_list.append('%05d' % int(elm))
+        except:
+            res_list.append(elm)
+    res= '-'.join(res_list)
+    return res
+
+#-----------------------------
+def complete_data(data_parsed):
+    out= data_parsed[:]
+
+    # filling totals
+    val_fields_list= [k for k, v in dbval_types.iteritems() if v in ['int','float']]
+    if len(val_fields_list) > 0:
+
+        total_dict= {} # prepare total dict
+        for field in val_fields_list:
+            total_dict[field]= 0
+        
+        print "... calculating totals"
+        for row in out:
+            if row['level'] == 'a':
+                for field in val_fields_list:
+                    total_dict[field] += row[field]
+        # finishing total dict
+        total_dict['idef']= '99999'
+        total_dict['parent']= None
+        total_dict['type']= 'Total'
+        total_dict['name']= 'GRAND TOTAL'
+        total_dict['level']= 'a'
+        out.append(total_dict)
+
+    print "... filling _sort keys"
+    for row in out:
+        row['idef_sort']= sort_format(row['idef'])
+        if row['parent'] is None:
+            row['parent_sort']= None
+        else:
+            row['parent_sort']= sort_format(row['parent'])
+        #simultaneously preparing to the next step
+        row['leaf']= True
+
+    print "... filling leaves"
+    data_children= out[:]
+    for data_row in out:
+        for data_child in data_children:
+            if data_row['idef'] == data_child['parent']:
+                data_row['leaf']= False
+                break
+
+    return out
 
 #-----------------------------------------------
 # parse CSV, convert it to JSON and save to file
@@ -68,15 +127,10 @@ def csv_parse(filename_csv, filename_schema, delimit, quote):
         filename_schema= filename_csv.rstrip('.csv')+'-schema.json'
     try:
         csv_src= open(filename_csv, 'rb')
-        sch_src= open(filename_schema, 'rb')
     except IOError as e:
         print 'Unable to open src_file:\n %s\n' % e
     else:
         csv_read= csv.reader(csv_src, delimiter= delimit, quotechar= quote)
-        
-        schema= json.load(sch_src, encoding='utf-8') # schema file
-        dbkey_alias= schema["alias"] # dict of aliases -> document keys in db
-        dbval_types= schema["type"] # dict of types -> values types in db
 
         for row in csv_read:
             keys= tuple(row)
@@ -125,7 +179,7 @@ if __name__ == "__main__":
     # process command line options
     cmdparser = optparse.OptionParser(usage="usage: python %prog [Options] src_filename.csv src_schema.json")
     cmdparser.add_option("-f", "--conf", action="store", dest="conf_filename", help="configuration file")
-    cmdparser.add_option("-t", "--collt", action="store", dest="collect_name", help="collection name")
+    cmdparser.add_option("-l", "--coll", action="store", dest="collect_name", help="collection name")
     cmdparser.add_option("-c", action="store_true",dest='dbact',help="clean db before insert (ignored if db is not updated)")
 
     opts, args = cmdparser.parse_args()
@@ -140,17 +194,29 @@ if __name__ == "__main__":
         print 'Unable to open file:\n %s\n' % e
         exit()
 
-    try: #read schema file
+    #schema file
+    try:
         filename_schema= args[1]
     except:
         filename_schema= None
     if filename_schema is None:
         filename_schema= args[0].rstrip('.csv')+'-schema.json'
 
+    try:
+        sch_src= open(filename_schema, 'rb')
+    except:
+        "ERROR: Can't open schema file, exiting now"
+        exit()
+
+    schema= json.load(sch_src, encoding='utf-8') # schema file
+    dbkey_alias= schema["alias"] # dict of aliases -> document keys in db
+    dbval_types= schema["type"] # dict of types -> values types in db
+
+
     conf_filename= opts.conf_filename
     if conf_filename is None:
-        print 'No configuration file is specified, exiting now'
-        exit()        
+        print "ERROR: Can't find configuration, exiting now"
+        exit()
 
     # get mongo connection details
     conn_mongo= get_db_connect(conf_filename, 'mongodb')
@@ -161,9 +227,9 @@ if __name__ == "__main__":
     try:
         connect= pymongo.Connection(conn_mongo_host, conn_mongo_port)
         mongo_db= connect[conn_mongo_db]
-        print '...connected to the database', mongo_db
+        print '... connected to the database', mongo_db
     except Exception as e:
-        print 'Unable to connect to the mongodb database:\n %s\n' % e
+        print 'ERROR: Unable to connect to the mongodb database:\n %s\n' % e
         exit()
 
     # authentication
@@ -172,7 +238,7 @@ if __name__ == "__main__":
     if conn_mongo_password is None:
         conn_mongo_password = getpass.getpass()
     if mongo_db.authenticate(conn_mongo_username, conn_mongo_password) != 1:
-        print 'Cannot authenticate to db, exiting now'
+        print 'ERROR: Cannot authenticate to db, exiting now'
         exit()
 
     clean_db= opts.dbact # False - insert() data, True - remove() and then insert()
@@ -180,27 +246,24 @@ if __name__ == "__main__":
     try:
         collect_name= opts.collect_name
     except:
-        print 'WARNING! No collection name specified!'
+        print 'ERROR: No collection name specified!'
         exit()
 
-    csv_delim= ';' #read CSV file with data
+    csv_delim= ';'
     csv_quote= '"'
-    
-    print '... Processing CSV file'
-    csv_data_parsed= csv_parse(args[0], filename_schema, csv_delim, csv_quote) #parse CSV and save it to dict
 
-    if len(csv_data_parsed) == 0:
+    print '... reading and parsing CSV file'
+    #parse CSV and save it to dict
+    obj_parsed= csv_parse(args[0], filename_schema, csv_delim, csv_quote)
+
+    if len(obj_parsed) != 0:
+        obj_final= complete_data(obj_parsed)
+    else:
         print "The file %s is empty" % src_file
         exit()
 
-    print '... Writing data to the database'
-    print db_insert(csv_data_parsed, mongo_db, collect_name, clean_db), 'records inserted'
-#     try:
-#         #rec_count= mongo_update(csv_data_parsed, dbparam[0], dbparam[1], clean_db_first)
-#         print '...inserting into db...\n ', db_insert(csv_data_parsed, mongo_db, collect_name, clean_db), 'records inserted'
-#     except:
-#         print 'Interrupting script: something went wrong while db insert! Check your db settings:'
-#         exit()
+    print '... inserting data to the database'
+    print db_insert(obj_final, mongo_db, collect_name, clean_db), 'records in the collection %s' % collect_name
 
     print 'Done'
         
