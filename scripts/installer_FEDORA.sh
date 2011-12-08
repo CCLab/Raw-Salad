d8e38f46c5869eecc712464f30a769c33b21b9aa
Bartosz Stalewski <bartekstalewski@gmail.com>
2011-12-08 12:21:30 +0100

    Total extermination

--- scripts/installer_FEDORA.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Install all the dependecies with yum
-su -c 'yum install git mongodb mongodb-server python pymongo python-simplejson Django'
--- scripts/js/todos.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd ../../src/rawsalad/site_media/js/
-grep -n 'TODO' * | sed 's/\ \ \+/\t/g'
